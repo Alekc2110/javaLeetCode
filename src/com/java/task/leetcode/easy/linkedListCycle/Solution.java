99adc4e4b11af3ecfb64f76199a5c999369ec72a
Aleksandr Korshunovych <korsunovica@gmail.com>
2023-11-02 16:05:28 +0200

    refactored linkedListCycle

--- src/com/java/task/leetcode/easy/linkedListCycle/Solution.java
+++ src/com/java/task/leetcode/easy/linkedListCycle/Solution.java
@@ -23,9 +23,24 @@ package com.java.task.leetcode.easy.linkedListCycle;
 public class Solution {
 
     public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(3);
+        ListNode node4 = new ListNode(4);
+        ListNode node5 = new ListNode(5);
+        ListNode node6 = new ListNode(6);
+        ListNode node7 = new ListNode(7);
+        head.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        node5.next = node6;
+        node6.next = node3; // cycling reference
+        System.out.println(hasCycle(head));
+
     }
 
-    public boolean hasCycle(ListNode head) {
+    public static boolean hasCycle(ListNode head) {
         ListNode slow = head;
         ListNode fast = head;
         while (fast != null && fast.next != null) {
